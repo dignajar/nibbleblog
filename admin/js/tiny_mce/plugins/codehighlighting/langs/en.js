3fa52afdd5bc192613f6f3fd4c239ea4f89f6bae
Diego Najar <dignajar@gmail.com>
2012-09-20 13:19:34 -0300

    20/09/2012

---
+++ admin/js/tiny_mce/plugins/codehighlighting/langs/en.js
@@ -0,0 +1,13 @@
+// UK lang variables
+tinyMCE.addI18n('en.codehighlighting',{
+codehighlighting_desc : "Code Highlighting",
+codehighlighting_title : "Code Highlighting",
+codehighlighting_languagepicker : "Choose the language",
+codehighlighting_pagecode : "Paste your code here",
+codehighlighting_button_desc: "Insert code",
+codehighlighting_nogutter : "No Gutter",
+codehighlighting_collapse : "Collapse Code",
+codehighlighting_nocontrols : "Hide Toolbar",
+codehighlighting_showcolumns : "Show Columns",
+codehighlighting_htmlscript : "HTML Script"
+});
