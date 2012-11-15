2c0a82966dc7183d6fef82b122b22ec56421b2fd
Diego Najar <dignajar@gmail.com>
2012-11-15 10:49:49 -0300

    Fixes on friendly url

--- admin/kernel/helpers/text.class.php
+++ admin/kernel/helpers/text.class.php
@@ -111,7 +111,8 @@ class Text {
 	// Clean text for URL
 	public static function clean_url($text)
 	{
-		$text = str_replace(array("!", "*", "&#039;", "(", ")", ";", ":", "@", "&amp", "=", "+", "$", ",", "/", "?", "%", "#", "[", "]", "|"),'',$text);
+		$text = str_replace(array("!", "*", "(", ")", ";", ":", "@", "&amp", "=", "+", "$", ",", "/", "?", "%", "#", "[", "]", "|"),'',$text);
+		//$text = str_replace(array("!", "*", "&#039;", "(", ")", ";", ":", "@", "&amp", "=", "+", "$", ",", "/", "?", "%", "#", "[", "]", "|"),'',$text);
 		$text = str_replace(" ","-",$text);
 		return($text);
 	}
