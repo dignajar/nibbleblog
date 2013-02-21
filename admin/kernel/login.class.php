fc7b5c50bffae92e6cb82da441da3d91b6eba5c6
Diego Najar <dignajar@gmail.com>
2013-02-21 12:07:41 -0300

    changes on themes

--- admin/kernel/login.class.php
+++ admin/kernel/login.class.php
@@ -107,7 +107,7 @@ class Login {
 	{
 		$_SESSION = array();
 
-		if (ini_get("session.use_cookies"))
+		if(ini_get("session.use_cookies"))
 		{
 			$params = session_get_cookie_params();
 			setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
